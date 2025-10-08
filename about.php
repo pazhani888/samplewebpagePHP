8ab5c746907cd1941a50d52740234fbee8738e7d
pazhani888 <pazhanisamysubramaniyan@gmail.com>
2025-10-08 12:34:21 -0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+// about.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>About This Site</h1>
+    <p>This is a simple PHP website deployed on Azure App Service.</p>
+    <a href="index.php">← Back to Home</a>
+</body>
+</html>
