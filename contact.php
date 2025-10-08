c833ed6c0cef1bcb94d858143661bd539b762e8d
pazhani888 <pazhanisamysubramaniyan@gmail.com>
2025-10-08 12:35:50 -0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+// contact.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Contact Page</h1>
+    <p>Email us at <strong>admin@example.com</strong></p>
+    <a href="index.php">← Back to Home</a>
+</body>
+</html>
